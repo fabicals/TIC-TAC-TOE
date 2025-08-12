e2c15bdbadb3c6575a9f66f954289bb64e892226
fabicals <mariashakeel888@gmail.com>
2025-08-12 04:38:11 +0400

    player buggy

--- public/game.js
+++ public/game.js
@@ -5,7 +5,8 @@ let currentTurn = "X";
 
 socket.on('player-assigned', (symbol) => {
     playerSymbol = symbol;
-    alert(`You are Player ${symbol}`);
+    document.getElementById('player-info').textContent = `You are Player ${symbol}`
+    updateTurnInfo();
 });
 
 socket.on('room-full', () => {
@@ -16,6 +17,7 @@ socket.on('move', ({ index, symbol }) => {
     board[index]= symbol;
     document.getElementById(`cell-${index}`).textContent=symbol;
     currentTurn = symbol === "X" ? "O" : "X";
+    updateTurnInfo();
 });
 
 document.querySelectorAll('.cell').forEach((cell, index) => {
@@ -25,6 +27,44 @@ document.querySelectorAll('.cell').forEach((cell, index) => {
             cell.textContent = playerSymbol;
             socket.emit('move', {index, symbol: playerSymbol});
             currentTurn = playerSymbol === "X" ? "O" : "X";
+            updateTurnInfo();
+            checkWin();
         }
     });
 });
+
+function updateTurnInfo(){
+    const turnDiv = document.getElementById("turn-info");
+    if (playerSymbol == currentTurn) {
+        turnDiv.textContent = "YOUR TURN"
+    } else {
+        turnDiv.textContent = `Waiting for player ${currentTurn}'s Turn`
+    }
+}
+
+function checkWin() {
+    const turnDiv = document.getElementById("turn-info");
+    const wins = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i = 0; i < wins.length; i++) {
+    const [a, b, c] = wins[i]
+
+    if (
+        board[a] !== null &&
+        board[a] == board[b] &&
+        board[a] == board[c]
+    )
+    turnDiv.textContent = 'YOU WIN!'
+    return true;
+  }
+
+}
--- server.js
+++ server.js
@@ -5,26 +5,34 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-let players = 0;
+const players = {};
 
 io.on('connection', (socket) => {
+    const playerCount = Object.keys(players).length;
 
-    if (players >= 2) {
+    if (playerCount >= 2) {
         socket.emit('room-full');
             return;
     }
 
-    const symbol = players === 0 ? 'X' : 'O';
-    players++;
+    const assignedSymbols = Object.values(players);
+    const symbol = assignedSymbols.includes ('X') ? 'O' : 'X';
+
+    players[socket.id] = symbol;
+    
+
     socket.emit('player-assigned', symbol);
+    io.emit('update-players', players);
+
 
     socket.on('move' , (data) => {
         socket.broadcast.emit('move',data);
     });
 
     socket.on('disconnect', () => {
-        players--;
+        delete players[socket.id];
         socket.broadcast.emit('player-disconnected');
+        io.emit('update-players', players);
     });
 
 });
